f0261192285707d5793657331cd202877586daa7
shadrackkiprotich <mylesshadrack99@gmail.com>
2020-11-20 16:28:52 +0300

    added some new files

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name: ");
+            string name=Console.ReadLine();
+            Console.WriteLine("Enter the Initial Balance :");
+            decimal  number=Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter your account Number:");
+            var num = Convert.ToInt32(Console.ReadLine());
+            BankAccount();
+            
+        }
+      
+    }
+    public class BankAccount
+    {
+        
+        public string owner { get; set; }
+        public int number { get; set; }
+        public decimal balance { get; set; }
+        public static string accountNumberSeed = 1234567890;
+       
+        public void MakeDeposit( decimal number, DateTime date, string note)
+        {
+            this.owner = name;
+            this.number = initialbalance;
+            this.number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+        }
+        public void MakeWithdrawal( decimal number, DateTime date, string note)
+        {
+
+        }
+    }
+        
+
+}
